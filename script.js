7c78a931b94b750efb90a291eddd5bff56009aec
inezakevin <inezakevin23@gmail.com>
2025-10-03 12:20:44 +0200

    I used event handling in jquery to create hide/show button

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+     //selecting div and change its content
+            $("#myDiv")
+            .html("<h1>Hello jQuery!</h1>")
+            .css({
+                color: "green",
+                "font-size": "18px",
+                "text-align": "center"
+            })
+            .fadeIn(1000);
+            //selecting paragraph and change its content
+            $("p")
+            .css({
+                "text-align" : "center"
+            })
+            .fadeIn(2000)
+            .delay(1000)
+            //used function to make first text of paragraph appear before changing it
+            .fadeOut(1000, function(){
+                $(this)
+                .text("I like jQuery")
+                .css({
+                    "background-color" : "grey",
+                    "font-size" : "15px"
+                })
+                .fadeIn(2000);
+            });
+            //button click event to hide and show section
+            $("Button").on("click", function(){
+                $("#container").toggle();
+            });
+});
